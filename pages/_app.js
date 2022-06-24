14d687ce74844b7060ef4a509a9e0e893a00646c
felipe.magagnin <felipebmagagnin@gmail.com>
2022-06-23 21:23:43 -0300

    Add rainbow kit and eslint to the project

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,74 @@
 import '../styles/globals.css'
 
+import '@rainbow-me/rainbowkit/styles.css';
+
+import {
+  connectorsForWallets,
+  getDefaultWallets,
+  RainbowKitProvider,
+  wallet
+} from '@rainbow-me/rainbowkit';
+import {
+  chain,
+  configureChains,
+  createClient,
+  WagmiConfig,
+} from 'wagmi';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
+import { jsonRpcProvider } from 'wagmi/providers/jsonRpc';
+
+const fantomTestnetChain = {
+  id: 0xfa2,
+  name: 'Fantom TestNet',
+  network: 'fantom',
+  iconUrl: 'https://example.com/icon.svg',
+  iconBackground: '#fff',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'Fantom',
+    symbol: 'FTM',
+  },
+  rpcUrls: {
+    default: 'https://rpc.testnet.fantom.network/',
+  },
+  blockExplorers: {
+    default: { name: 'Fantom Scan', url: 'https://testnet.ftmscan.com/' },
+  },
+  testnet: true,
+};
+
+
+const { chains, provider } = configureChains(
+  [fantomTestnetChain],
+  [jsonRpcProvider({ rpc: chain => ({ http: chain.rpcUrls.default }) })]
+);
+
+const connectors = connectorsForWallets(
+  [
+    {
+      groupName: 'Recommended',
+      wallets: [
+        wallet.metaMask({ chains }),
+      ],
+    }
+  ]
+);
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider
+})
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains} coolMode>
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
 }
 
 export default MyApp
